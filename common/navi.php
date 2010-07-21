f062ce075023a7f330926e5e7871b5a64d5cd0c9
Mizuki Yamanaka <charles.vineyard@gmail.com>
2010-07-21 13:26:17 +0900

    左メニュー、見出し部分（画像）にもindexへのリンクつけた

--- common/navi.php
+++ common/navi.php
@@ -13,21 +13,21 @@ $rootpath = $GLOBALS["rootpath"];
 
             <li>
                 <dl>
-                    <dt><img src="<?php print $rootpath; ?>images/h_menu_directory.jpg" alt="サイト構造" width="150" height="30" /></dt>
+                    <dt><a href="<?php print $rootpath; ?>directory/index.php"><img src="<?php print $rootpath; ?>images/h_menu_directory.jpg" alt="サイト構造" width="150" height="30" /></a></dt>
                         <dd<?php if ($active == 'directory') print ' id="active"'; ?>><a href="<?php print $rootpath; ?>directory/index.php">カテゴリー・ページの一覧</a></dd>
                 </dl>
             </li>
 
             <li>
                 <dl>
-                    <dt><img src="<?php print $rootpath; ?>images/h_menu_category.jpg" alt="カテゴリー" width="150" height="30" /></dt>
+                    <dt><a href="<?php print $rootpath; ?>category/index.php"><img src="<?php print $rootpath; ?>images/h_menu_category.jpg" alt="カテゴリー" width="150" height="30" /></a></dt>
                         <dd<?php if ($active == 'category') print ' id="active"'; ?>><a href="<?php print $rootpath; ?>category/index.php">追加・編集・削除</a></dd>
                 </dl>
             </li>
 
             <li>
                 <dl>
-                    <dt><img src="<?php print $rootpath; ?>images/h_menu_page.jpg" alt="ページ" width="150" height="30" /></dt>
+                    <dt><a href="<?php print $rootpath; ?>page/index.php"><img src="<?php print $rootpath; ?>images/h_menu_page.jpg" alt="ページ" width="150" height="30" /></a></dt>
                         <dd<?php if ($active == 'pageContribution') print ' id="active"'; ?>><a href="<?php print $rootpath; ?>page/contribution/index.php">新規作成</a></dd>
                         <dd<?php if ($active == 'page') print ' id="active"'; ?>><a href="<?php print $rootpath; ?>page/index.php">編集・削除</a></dd>
                 </dl>
@@ -35,20 +35,20 @@ $rootpath = $GLOBALS["rootpath"];
 
             <li>
                 <dl>
-                    <dt><img src="<?php print $rootpath; ?>images/h_menu_tag.jpg" alt="タグ" width="150" height="30" /></dt>
+                    <dt><a href="<?php print $rootpath; ?>tag/index.php"><img src="<?php print $rootpath; ?>images/h_menu_tag.jpg" alt="タグ" width="150" height="30" /></a></dt>
                         <dd<?php if ($active == 'tag') print ' id="active"'; ?>><a href="<?php print $rootpath; ?>tag/index.php">追加・編集・削除</a></dd>
                 </dl>
             </li>
             <li>
                 <dl>
-                    <dt><img src="<?php print $rootpath; ?>images/h_menu_file.jpg" alt="ファイル" width="150" height="30" /></dt>
+                    <dt><a href="<?php print $rootpath; ?>file/index.php"><img src="<?php print $rootpath; ?>images/h_menu_file.jpg" alt="ファイル" width="150" height="30" /></a></dt>
                         <dd<?php if ($active == 'file') print ' id="active"'; ?>><a href="<?php print $rootpath; ?>file/index.php">追加・編集・削除</a></dd>
                 </dl>
             </li>
 
             <li>
                 <dl>
-                    <dt><img src="<?php print $rootpath; ?>images/h_menu_site.jpg" alt="サイト全般" width="150" height="30" /></dt>
+                    <dt><a href="<?php print $rootpath; ?>site/index.php"><img src="<?php print $rootpath; ?>images/h_menu_site.jpg" alt="サイト全般" width="150" height="30" /></a></dt>
                         <dd<?php if ($active == 'site') print ' id="active"'; ?>><a href="<?php print $rootpath; ?>site/index.php">サイト情報の編集</a></dd>
                 </dl>
             </li>
